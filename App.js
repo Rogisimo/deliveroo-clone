764a7abf4f480ee2910c4a549abab28f78010ade
Rogisimo <rogisonfleek@gmail.com>
2023-11-23 14:54:57 +0200

    DeliveryScreen

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import {store} from"./store"
 import BasketScreen from './screens/BasketScreen';
 import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +26,9 @@ export default function App() {
       options={{presentation:"modal", headerShown:false}}
       />
       <Stack.Screen name='PreparingOrder' component={PreparingOrderScreen}
+      options={{presentation:"fullScreenModal", headerShown:false}}
+      />
+      <Stack.Screen name='Delivery' component={DeliveryScreen}
       options={{presentation:"fullScreenModal", headerShown:false}}
       />
     </Stack.Navigator>
